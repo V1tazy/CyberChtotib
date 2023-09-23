d145d4dce5f2341208de6703751a8f21ac2b7bfa
Borikmm <valera.nalabordin000@mail.ru>
2023-09-23 13:38:04 +0900

    v.0.4
    v.0.4

--- Assets/Scripts/Move_player.cs
+++ Assets/Scripts/Move_player.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Move_player : MonoBehaviour
 {
     
     BoxCollider2D box;
     SpriteRenderer sr;
+    private TMP_Text moneyText; //field money 
 
     public float speed = 5f;
 
@@ -14,6 +18,7 @@ public class Move_player : MonoBehaviour
     {
         box = GetComponent<BoxCollider2D>();
         sr =  GetComponent<SpriteRenderer>();
+        moneyText = GameObject.FindGameObjectWithTag("HP").GetComponent<TMP_Text>();
     }
 
     private void Update()
@@ -27,6 +32,19 @@ public class Move_player : MonoBehaviour
         }
 
         transform.position += new Vector3(movement_x, movement_y, 0) * speed * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(0, 0, 0);
 
+    }
+
+    void OnCollisionEnter2D()
+    {
+        if (int.Parse(moneyText.text) > 1 )
+        {
+            moneyText.text =  (int.Parse(moneyText.text) - 1).ToString();
+        }
+        else{
+            SceneManager.LoadScene(0);
+        }
+        
     }
 }
